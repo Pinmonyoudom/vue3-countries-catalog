d286e61ad33df9af4c0b889d97a91f4ba019b7a3
Pinmony Oudom <pinmonyoudom@Pinmonys-MacBook-Pro.local>
2024-01-11 01:02:32 +0700

    remove comment

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,3 @@
-// import Vue from 'vue';
-// import Vuetify from 'vuetify/lib/framework';
-
-// Vue.use(Vuetify);
-
-// export default new Vuetify({
-// });
 import Vue from 'vue';
 import '@mdi/font/css/materialdesignicons.css'
 import Vuetify from 'vuetify/lib/framework';
